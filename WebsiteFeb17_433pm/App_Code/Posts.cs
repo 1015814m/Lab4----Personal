7444ac169da4e98146da4c2ba1ebf4c59fd8f1b1
1015814m <1015814morrissey@gmail.com>
2018-02-21 18:36:14 -0500

    Add files via upload

---
+++ WebsiteFeb17_433pm/App_Code/Posts.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// 
+/// </summary>
+public class Posts
+{
+    private int postID;
+    private string post;
+    private DateTime postTime;
+    private int likes;
+    private int employeeID;
+    private int achievementID;
+    /// <summary>
+    /// For creating a post
+    /// </summary>
+    /// <param name="postID"></param>
+    /// <param name="post"></param>
+    /// <param name="postTime"></param>
+    /// <param name="likes"></param>
+    /// <param name="employeeID"></param>
+    /// <param name="achievementID"></param>
+    public Posts(int postID, string post, DateTime postTime, int likes, int employeeID, int achievementID)
+    {
+        PostID = postID;
+        Post = post;
+        PostTime = postTime;
+        Likes = likes;
+        EmployeeID = employeeID;
+        AchievementID = achievementID;
+    }
+
+    public int PostID
+    {
+        get
+        {
+            return postID;
+        }
+        private set
+        {
+            postID = value;
+        }
+    }
+
+    public string Post
+    {
+        get
+        {
+            return post;
+        }
+        private set
+        {
+            post = value;
+        }
+    }
+
+    public DateTime PostTime
+    {
+        get
+        {
+            return postTime;
+        }
+        private set
+        {
+            postTime = value;
+        }
+    }
+
+    public int Likes
+    {
+        get
+        {
+            return likes;
+        }
+        private set
+        {
+            likes = value;
+        }
+    }
+
+    public int EmployeeID
+    {
+        get
+        {
+            return employeeID;
+        }
+        private set
+        {
+            employeeID = value;
+        }
+    }
+
+    public int AchievementID
+    {
+        get
+        {
+            return achievementID;
+        }
+        private set
+        {
+            achievementID = value;
+        }
+    }
+}
